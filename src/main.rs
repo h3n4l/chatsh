0a1b6540ba2aa3ef2af2acbe16683cf45daf3278
h3n4l <oysterdays@gmail.com>
2023-06-10 01:11:08 +0800

    chore: report error if cannot get environment variable

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+use colored::*;
 use std::env;
 
 use chatsh::app;
 
 fn main() {
-    let openai_key = env::var("OPENAI_KEY").expect("OPENAI_KEY is not set.");
+    let openai_key = env::var("OPENAI_KEY");
+    if openai_key.is_err() {
+        println!(
+            "{}",
+            format!(
+                "Cannot get OPENAI_KEY environment variable, error: {}",
+                openai_key.err().unwrap()
+            )
+            .red()
+        );
+        std::process::exit(1);
+    }
+
+    let openai_key = openai_key.unwrap();
     let mut app = app::App::new(openai_key.as_str());
     app.run();
 }
